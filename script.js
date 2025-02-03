289128a2e414ee6ab2fc443dc0550ea7426394da
ThiiagoGoncalves <thiagosantosbr294@gmail.com>
2025-02-03 15:11:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+let index = 0;
+const slides = document.querySelectorAll('.card');
+const lastSlideIndex = slides.length - 1; 
+slides[index].style.display = 'block';
+
+function updateSlide() {
+    slides.forEach(slide => slide.style.display = 'none');
+    slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    updateSlide();
+    if (index === lastSlideIndex) {
+        document.querySelector('.btn-left').style.display = 'none';
+        document.querySelector('.btn-right').style.display = 'none';
+    }
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    updateSlide();
+    if (index !== lastSlideIndex) {
+        document.querySelector('.btn-left').style.display = 'block';
+        document.querySelector('.btn-right').style.display = 'block';
+    }
+}
+
+function playMusic() {
+    document.getElementById('background-music').play();
+}
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.innerHTML = '❤️';
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (Math.random() * 3 + 2) + 's';
+    document.body.appendChild(heart);
+    setTimeout(() => heart.remove(), 5000);
+}
+
+setInterval(createHeart, 500);
